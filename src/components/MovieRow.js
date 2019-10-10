768898d55ee33fa30ea6febe4ed72d504c3eadfc
Breyanna Vass <breyannavass@gmail.com>
2019-10-09 21:44:37 -0400

    adding a separate view button for each movie

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 class MovieRow extends React.Component {
+  viewMovie() {
+    // console.log("Trying to view movie");
+    // console.log(this.props.movie.title);
+    const url = "https://www.themoviedb.org/movie/" + this.props.movie.id;
+    window.location.href = url;
+  }
+
   render() {
     return (
       <table key={this.props.movie.id}>
@@ -10,8 +17,13 @@ class MovieRow extends React.Component {
               <img alt="poster" width="120" src={this.props.movie.poster_src} />
             </td>
             <td>
-              {this.props.movie.title}
+              <h3>{this.props.movie.title}</h3>
               <p>{this.props.movie.overview}</p>
+              <input
+                type="button"
+                onClick={this.viewMovie.bind(this)}
+                value="View"
+              />
             </td>
           </tr>
         </tbody>
