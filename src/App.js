7b2f62e3a1d1964fe9917ce253fddc867313cdda
Breyanna Vass <breyannavass@gmail.com>
2019-10-09 20:24:12 -0400

    adding contructor and state to the app js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <table className="titleBar">
-        <tbody>
-          <tr>
-            <td>
-              <img alt="app icon" width="50" src="green_app_icon.svg" />
-            </td>
-            <td>
-              <h1>MoviesDB Search</h1>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("This is my initializer");
+    this.state = {
+      rows: <p>This is my row</p>
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img alt="app icon" width="50" src="green_app_icon.svg" />
+              </td>
+              <td>
+                <h1>MoviesDB Search</h1>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input
+          style={{
+            fontSize: 24,
+            display: "block",
+            width: "99%",
+            paddingTop: 8,
+            paddingBottom: 8,
+            paddingLeft: 16
+          }}
+          placeholder="Enter seach term"
+        />
+        {this.state.rows}
+      </div>
+    );
+  }
 }
 
 export default App;
